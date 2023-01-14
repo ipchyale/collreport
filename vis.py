37bd1d6106ee45d1874dd48a9471600877bc1f9e
damoncrockett <damoncrockett@hotmail.com>
2023-01-14 16:12:53 -0500

    glyph legend color icon map

--- vis.py
+++ vis.py
@@ -1,6 +1,7 @@
 from PIL import Image,ImageDraw,ImageFont
 import os
 import textwrap
+import numpy as np
 
 FONTDIR = os.path.expanduser("~") + "/fonts/"
 
@@ -22,7 +23,7 @@ fonts = {
 
 font = ImageFont.truetype(fonts['regular'],45)
 font_large = ImageFont.truetype(fonts['regular'],90)
-font_serif = ImageFont.truetype(fonts['serif'],30)
+font_serif = ImageFont.truetype(fonts['serif'],36)
 
 def slider(title,val,vmin,vmax,font=font,theme='light'):
     
@@ -81,7 +82,11 @@ def gmat(im,font=font,theme='light',top='WARM',right='MATTE',bottom='ROUGH',left
         im.thumbnail((960,960),Image.Resampling.LANCZOS)
     
     gmat = Image.new('RGB',(1280,1280),bg)
-    gmat.paste(im,(160,160),im)
+    
+    try:
+        gmat.paste(im,(160,160),im)
+    except:
+        gmat.paste(im,(160,160))
     
     draw = ImageDraw.Draw(gmat)
     
@@ -110,16 +115,19 @@ def info_panel(idx,title,font=font,font_large=font_large,theme='light'):
     
     fontWidthIdx,fontHeightIdx = font_large.getsize(idx)
     fontWidthTitle,fontHeightTitle = font.getsize(title)
+
+    spacer = font_large.getsize("M")[0] # M is the widest character in a font, usually
+    panelWidth = fontWidthIdx + fontWidthTitle + spacer
     
-    canvas = Image.new('RGB',(fontWidthIdx*2+fontWidthTitle,fontHeightIdx),bg)
+    canvas = Image.new('RGB',(panelWidth,fontHeightIdx),bg)
     draw = ImageDraw.Draw(canvas)
     
     draw.text((0,0),text=idx,font=font_large,fill=fill)
-    draw.text((fontWidthIdx + int(fontWidthIdx/4),fontHeightIdx-fontHeightTitle),text=title,font=font,fill=fill)
+    draw.text((fontWidthIdx+spacer,fontHeightIdx-fontHeightTitle),text=title,font=font,fill=fill)
     
     return canvas
 
-def item_report(df,i,glyphcol,pcol,xcol,tcol,gcol,wcol,rcol,idx_title,idx_subtitle,theme='light'):
+def item_report(df,i,glyphcol,pcol,xcol,tcol,gcol,wcol,rcol,tonecol,contrastcol,mapcol,dminhexcol,dmaxhexcol,idx_title,idx_subtitle,theme='light'):
     
     if theme=='light':
         bg = "white"
@@ -136,13 +144,19 @@ def item_report(df,i,glyphcol,pcol,xcol,tcol,gcol,wcol,rcol,idx_title,idx_subtit
     cmax = round(df[wcol].max())
     rmin = round(df[rcol].min(),1)
     rmax = round(df[rcol].max(),1)
+    tonemin = round(df[tonecol].min())
+    tonemax = round(df[tonecol].max())
+    contrastmin = round(df[contrastcol].min()*100)
+    contrastmax = round(df[contrastcol].max()*100)
     
-    t,g,c,r = df[tcol].loc[i],round(df[gcol].loc[i]),round(df[wcol].loc[i]),round(df[rcol].loc[i],1)
+    t,g,c,r,tn,ctrst = df[tcol].loc[i],round(df[gcol].loc[i]),round(df[wcol].loc[i]),round(df[rcol].loc[i],1),round(df[tonecol].loc[i]),round(df[contrastcol].loc[i]*100)
     
     sliders = slider_panel(slider('THICKNESS (mm)',t,tmin,tmax,font,theme=theme),
                            slider('GLOSS (GU)',g,gmin,gmax,font,theme=theme),
-                           slider('WARMTH (b*)',c,cmin,cmax,font,theme=theme),
-                           slider('ROUGHNESS (σ)',r,rmin,rmax,font,theme=theme))
+                           slider('BASE WARMTH (b*)',c,cmin,cmax,font,theme=theme),
+                           slider('ROUGHNESS (σ)',r,rmin,rmax,font,theme=theme),
+                           slider('TONE WARMTH (b*)',tn,tonemin,tonemax,font,theme=theme),
+                           slider('CONTRAST (%)',ctrst,contrastmin,contrastmax,font,theme=theme))
     
     matted_glyph = gmat(glyph,theme=theme)
     matted_glyph.thumbnail((sliders.width,sliders.width),Image.Resampling.LANCZOS)
@@ -157,14 +171,24 @@ def item_report(df,i,glyphcol,pcol,xcol,tcol,gcol,wcol,rcol,idx_title,idx_subtit
     
     print_image.thumbnail((1024,1024),Image.Resampling.LANCZOS)
     texture_image.thumbnail((1024,1024),Image.Resampling.LANCZOS)
+
+    contrast_map = Image.open(df[mapcol].loc[i])
+    dminhex = df[dminhexcol].loc[i]
+    dmaxhex = df[dmaxhexcol].loc[i]
+    cicon = coloricon(dminhex,dmaxhex,s=256)
+
+    color_vis = Image.new('RGB', (cicon.width+contrast_map.width+64, cicon.height), bg)
+    color_vis.paste(cicon,(0,0))
+    color_vis.paste(contrast_map,(cicon.width+64,0))
     
-    images = Image.new('RGB',(1024,print_image.height+texture_image.height+64),bg)
+    images = Image.new('RGB',(1024,print_image.height+texture_image.height+64+color_vis.height+64),bg)
     
     images.paste(print_image,(0,0))
     images.paste(texture_image,(0,print_image.height+64))
+    images.paste(color_vis,(0,print_image.height+texture_image.height+64+64))
     
     infopanel = info_panel(idx_title,idx_subtitle)
-    infopanel.thumbnail((1024,1024),Image.Resampling.LANCZOS)
+    infopanel.thumbnail((48/infopanel.height*infopanel.width,48),Image.Resampling.LANCZOS)
     
     images_and_infopanel = Image.new('RGB',(1024,images.height+infopanel.height+64),bg)
     images_and_infopanel.paste(infopanel,(0,0))
@@ -228,6 +252,59 @@ def mat(im,theme='light'):
     matted.paste(im,(int(im.width * 0.1),int(im.height * 0.1)))
 
     return matted
+
+def glyph_legend(side=200,theme='light'):
+
+    if theme=='light':
+        bg = "white"
+    elif theme=='dark':
+        bg = "#212121"
+    
+    fill = "grey"
+    
+    im = Image.new('RGB', (side,side), bg)
+    draw = ImageDraw.Draw(im)
+    adj = int( side / 20 )
+    halfside = int( side / 2 )
+
+    draw.line([(halfside,0),(halfside,side)],
+              fill=fill,
+              width=round(side/200))
+    draw.line([(0,halfside),(side,halfside)],
+              fill=fill,
+              width=round(side/200))
+
+    sSixth = side / 6;
+    sThird = side / 3;
+    sHalf = side / 2;
+    sTwoThird = side * 2/3;
+    sFiveSixth = side * 5/6;
+
+    draw.line([(sHalf,0),(side,sHalf)],fill=fill,width=round(side/200))
+    draw.line([(side,sHalf),(sHalf,side)],fill=fill,width=round(side/200))
+    draw.line([(sHalf,side),(0,sHalf)],fill=fill,width=round(side/200))
+    draw.line([(0,sHalf),(sHalf,0)],fill=fill,width=round(side/200))
+
+    draw.line([(sHalf,sSixth),(sFiveSixth,sHalf)],fill=fill,width=round(side/200))
+    draw.line([(sFiveSixth,sHalf),(sHalf,sFiveSixth)],fill=fill,width=round(side/200))
+    draw.line([(sHalf,sFiveSixth),(sSixth,sHalf)],fill=fill,width=round(side/200))
+    draw.line([(sSixth,sHalf),(sHalf,sSixth)],fill=fill,width=round(side/200))
+
+    draw.line([(sHalf,sThird),(sTwoThird,sHalf)],fill=fill,width=round(side/200))
+    draw.line([(sTwoThird,sHalf),(sHalf,sTwoThird)],fill=fill,width=round(side/200))
+    draw.line([(sHalf,sTwoThird),(sThird,sHalf)],fill=fill,width=round(side/200))
+    draw.line([(sThird,sHalf),(sHalf,sThird)],fill=fill,width=round(side/200))
+
+    return gmat(mat(im))
+
+def coloricon(base,tone,s):
+    im = Image.new('RGB',(s,s),base)
+    draw = ImageDraw.Draw(im)
+    draw.rounded_rectangle(
+        [(s/2 - s/4, s/2 - s/4), (s/2 + s/4, s/2 + s/4)],radius=int(s*0.15625),fill=tone
+    )
+    
+    return im
 
 
 
